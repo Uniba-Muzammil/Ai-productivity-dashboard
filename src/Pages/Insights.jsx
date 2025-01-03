20fec589ad5887fd3558a7408f85197b58877153
Uniba-Muzammil <unibamuzammil8@gmail.com>
2025-01-03 20:53:05 +0500

    updated

---
+++ src/Pages/Insights.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function Insights() {
+  return (
+    <h2>Insights Page</h2>
+  );
+}
---
+++ src/Pages/Tasks.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function Tasks() {
+  return (
+    <h2>Tasks Page</h2>
+  );
+}
